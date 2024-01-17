b312664cd43da488e4c7fa701577e0cea0e5022a
Boglarka Mate <boglarka.mate@nmbu.no>
2024-01-17 13:40:25 +0100

    Changed print statement, and changed it to print a shorter path

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         for filename in os.listdir(folder_path):
             if filename.endswith('.toml'):
                 config_path = os.path.join(folder_path, filename)
-                print(f"Processing file: {config_path}")
+                print(f"Processing file: {os.path.basename(config_path)}")
 
                 # Read config from the current file in the loop
                 config_settings, config_layers = read_config(config_path)
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     elif file:
         # If a specific file is provided, process only that file
         file_path = os.path.abspath(file)
-        print(f"Processing file: {file_path}")
+        print(f"Processing file: {os.path.basename(file_path)}")
 
         # Read config from the specified file
         config_settings, config_layers = read_config(file_path)
@@ -50,4 +50,6 @@ if __name__ == "__main__":
         training_nn(config_layers, lr, batch_size, num_epochs)
 
     else:
-        print("Please provide either -d (or --folder) or -f (or --file) argument.")
+        print("Please provide either -d (or --folder) or -d (or --folder) and -f (or --file) argument.")
+        print("E.g: python file_name.py -d path_name")
+        print("E.g: python file_name.py -d path_name -f file_name.toml")
