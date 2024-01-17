9f901fc304d8f3d2abc062a791a65513006d4643
Boglarka Mate <boglarka.mate@nmbu.no>
2024-01-17 11:29:12 +0100

    Changed config file epoch to 1, made loop for reading all files in a folder

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ from src.classes.train import training_nn
 # With config read function
 
 # Making sure that we read config file from config folder
-config_settings, config_layers = read_config('config/our_config.toml')
+# config_settings, config_layers = read_config('config/our_config.toml')
 
-lr = config_settings.get('learningRate', 0.0001)
-batch_size = config_settings.get('batchSize', 64)
-num_epochs = config_settings.get('numEpochs', 10)
+# lr = config_settings.get('learningRate', 0.0001)
+# batch_size = config_settings.get('batchSize', 64)
+# num_epochs = config_settings.get('numEpochs', 10)
 
-training_nn(config_layers, lr, batch_size, num_epochs)
+# training_nn(config_layers, lr, batch_size, num_epochs)
+
+config_folder_path = 'config/'
+
+folder = os.listdir(config_folder_path)
+
+for file in folder:
+    if file.endswith('.toml'):
+        config_path = os.path.join(config_folder_path, file)
+
+        # Making sure that we read config file from config folder
+        config_settings, config_layers = read_config('config/our_config.toml')
+
+        lr = config_settings.get('learningRate', 0.0001)
+        batch_size = config_settings.get('batchSize', 64)
+        num_epochs = config_settings.get('numEpochs', 10)
+
+        training_nn(config_layers, lr, batch_size, num_epochs)
+
