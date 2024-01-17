6ab14dec7294a3a117d02e8097c14696f6bc5cd7
Dolly Tveter <celine.sophie.dolly.tveter@nmbu.no>
2024-01-17 11:43:40 +0100

    Made sure it loops through all the files and not the same one. Put in a print statement

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ import os
 from src.classes.read_config import read_config 
 from src.classes.train import training_nn
 
-# Final Validation Accuracy 88.83%
-# The best working code at the moment without orthonormal U and V
-# With config read function
+# The best working code at the moment.
+#    Based on regular U and V (not orthonormal)
 
-# Making sure that we read config file from config folder
-# config_settings, config_layers = read_config('config/our_config.toml')
+# File patch to config file folder
+folder_path = 'config/'
 
-# lr = config_settings.get('learningRate', 0.0001)
-# batch_size = config_settings.get('batchSize', 64)
-# num_epochs = config_settings.get('numEpochs', 10)
+# List of files in the config folder
+folder = os.listdir(folder_path)
 
-# training_nn(config_layers, lr, batch_size, num_epochs)
 
-config_folder_path = 'config/'
-
-folder = os.listdir(config_folder_path)
-
+# Loop for reading all the config files in a folder
 for file in folder:
     if file.endswith('.toml'):
-        config_path = os.path.join(config_folder_path, file)
+        # Connects folder path with file name 
+        # Eg: 'config/config_1.toml'
+        config_path = os.path.join(folder_path, file)
+
+        # Print the current file being processed
+        print(f"Processing file: {config_path}")
 
         # Making sure that we read config file from config folder
         config_settings, config_layers = read_config(config_path)
