f4cd9bfc12e68e64fb58cf8a1616af50939884d3
Dolly Tveter <celine.sophie.dolly.tveter@nmbu.no>
2024-01-18 10:20:37 +0100

    Made comments on read_config

--- src/classes/read_config.py
+++ src/classes/read_config.py
@@ -1,6 +1,15 @@
 import toml
 
+
 def read_config(name):
+    """
+    Returns the settings and the layers from a config file. 
+    Args:
+        name: name of the config file
+
+    Returns:
+        output settings and layers for given input
+    """
     with open(name, 'r') as file:
         conf = toml.load(file)
     
