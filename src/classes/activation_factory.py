0b48cdf0012d28e51921576a5e69f875833aff89
Dolly Tveter <celine.sophie.dolly.tveter@nmbu.no>
2024-01-18 15:14:54 +0100

    Made comments on Activation_class

--- src/classes/activation_factory.py
+++ src/classes/activation_factory.py
@@ -1,6 +1,11 @@
 import torch.nn as nn
 
+
 class Activation_factory:
+    """
+    A class for managing and providing access to various activation functions.
+    """
+
     def __init__(self):
         self._function_type = {
             'relu': nn.ReLU(),
@@ -10,16 +15,29 @@ class Activation_factory:
             'softmax': nn.Softmax()
         }
 
-    # Function for adding activation functions
     def register(self, key, name):
-        # Checking if the activation function is alredy in _activation_type. If not, it is added
+        """
+        Registers a new activation function with a given key
+        Args:
+            key (str):        the name of the activation function
+            name (nn.Module): The PyTorch activation function
+        """
+
+        # Checking if the activation function alredy is in _activation_type. If not, it is added
         if key not in self._function_type:
             self._function_type[key] = name
         
         else: 
             print(f"{key} activation function already exists.")
 
+
     # Calls the activation function
     def __call__(self, function):
+        """
+        Retrieves and returns the activation function associated with the provided key.
+        Args:
+            function (str): The key representing the activation function.
+        Returns:
+            torch.nn.Module: The PyTorch activation function.
+        """
         return self._function_type[function]
- 
