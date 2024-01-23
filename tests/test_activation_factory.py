3ae74f09eea5a064b2b1d88dc4d4d2f03b2659bf
Dolly Tveter <celine.sophie.dolly.tveter@nmbu.no>
2024-01-23 14:44:19 +0100

    Fixed Activation_factory test

--- tests/test_activation_factory.py
+++ tests/test_activation_factory.py
@@ -28,19 +28,19 @@ def test_activation_factory(create_activation_factory, key, function):
     # Check that the activation function is an instance of nn.LeakyReLU
     assert leaky_relu_activation == function
 
-def test_registrating_existing_activation_raise_error():
+# Testing if the ValueError is being raised
+def test_registrating_existing_activation_raise_error(create_activation_factory):
     # Arrange
     activation_factory = create_activation_factory
-
-    # Act: Register an activation function
-    activation_factory.register('relu', nn.ReLU())
 
     # Assert: Use pytest.raises to check if ValueError is raised
     with pytest.raises(ValueError, match="relu activation function already exists."):
         # Act: Attempt to register the same activation function again with the correct key
         activation_factory.register('relu', nn.ReLU())
 
-def test_call_nonexistent_activation():
+
+# Testing if the KeyError is being raised
+def test_call_nonexistent_activation(create_activation_factory):
     # Arrange
     activation_factory = create_activation_factory
 
