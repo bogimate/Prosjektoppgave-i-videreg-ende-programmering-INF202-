8f29aceaaadaf1815846e1e0bf12e4ebbed539bc
Boglarka Mate <boglarka.mate@nmbu.no>
2024-01-23 11:15:39 +0100

    Lagt til noen funksjoner på test training

--- tests/test_train.py
+++ tests/test_train.py
@@ -5,6 +5,9 @@ from src.classes.read_config import read_config
 from src.classes.neural_network import Neural_network
 from src.classes.load_MNIST import load_MNIST
 
+# Hva skal man egentlig teste på training loopen?
+# Hva skal man egentlig teste på neural network?
+# Hvorfor funker ikke den ene activation testen vår?
 
 @pytest.fixture
 def real_data_loader():
@@ -13,3 +16,9 @@ def real_data_loader():
 def test_training_nn(real_data_loader):
     # Load configuration from a test file s
     config_settings, config_layers = read_config("config/config_test.toml")
+
+    # Create a real neural network based on config_layers and lr from config_settings
+    neural_net = Neural_network(config_layers, config_settings.get('learningRate', 0.001))
+
+    # Call the training function with the real data loader and neural network
+    training_nn(config_settings, config_layers, real_data_loader, output_file=None)
